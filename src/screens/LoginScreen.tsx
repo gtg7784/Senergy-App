fcb0ac1d74c15aa0509c6409833afb9e075d69c1
gtg7784 <gtg7784@naver.com>
2020-06-28 16:39:26 +0900

    update login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,13 +1,8 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  TouchableHighlight,
-} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import Container from '../components/Container';
+import Button from '../components/Button';
 import LoginModal from '../components/LoginModal';
 import colors from '../constants/colors';
 import images from '../constants/images';
@@ -38,15 +33,7 @@ const styles = StyleSheet.create({
     width: 260,
     height: 170,
     marginTop: 80,
-  },
-  button: {
-    width: 295,
-    height: 55,
-    borderRadius: 14,
-    backgroundColor: colors.YELLOW,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 102,
+    marginBottom: 102,
   },
   buttonText: {
     fontSize: 20,
@@ -56,8 +43,11 @@ const styles = StyleSheet.create({
     color: colors.BLACK,
   },
   bottomWrap: {
+    width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 40,
     marginTop: 40,
   },
   bottomText: {
@@ -66,9 +56,9 @@ const styles = StyleSheet.create({
     fontStyle: 'normal',
     letterSpacing: 0,
     color: colors.WHITE,
+    marginRight: 6,
   },
   bottomBtnText: {
-    marginLeft: 6,
     fontSize: 14,
     fontWeight: 'normal',
     fontStyle: 'normal',
@@ -77,31 +67,38 @@ const styles = StyleSheet.create({
   },
 });
 
-const LoginScreen = () => {
+interface Props {
+  navigation: StackNavigationProp<MainParamList, 'LoginScreen'>;
+}
+
+const LoginScreen: React.FC<Props> = ({navigation}) => {
   const [isModal, setIsModal] = useState(false);
+
+  const onRouteRegister = () => {
+    setIsModal(false);
+    navigation.navigate('RegisterScreen');
+  };
+
   return (
     <>
-      <View style={styles.container}>
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.title}>SENERGY</Text>
-          <Text style={styles.subTitle}>하루 5초, 24시간을 유용하게</Text>
-          <Image style={styles.illust} source={images.mainIllust} />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => setIsModal(true)}>
-            <Text style={styles.buttonText}>로그인</Text>
-          </TouchableOpacity>
-          <View style={styles.bottomWrap}>
-            <Text style={styles.bottomText}>
-              아직 시너지를 사용하고 있지 않으신가요?
-            </Text>
-            <TouchableHighlight>
-              <Text style={styles.bottomBtnText}>가입하기</Text>
-            </TouchableHighlight>
-          </View>
-        </SafeAreaView>
-      </View>
+      <Container style={styles.container}>
+        <Text style={styles.title}>SENERGY</Text>
+        <Text style={styles.subTitle}>하루 5초, 24시간을 유용하게</Text>
+        <Image style={styles.illust} source={images.mainIllust} />
+        <Button label="로그인" onPress={() => setIsModal(true)} />
+        <View style={styles.bottomWrap}>
+          <Text style={styles.bottomText}>
+            아직 시너지를 사용하고 있지 않으신가요?
+          </Text>
+          <TouchableHighlight
+            underlayColor={colors.TRANSPARENT}
+            onPress={() => onRouteRegister()}>
+            <Text style={styles.bottomBtnText}>가입하기</Text>
+          </TouchableHighlight>
+        </View>
+      </Container>
       <LoginModal
+        navigation={navigation}
         isVisible={isModal}
         changeValue={() => setIsModal(!isModal)}
       />
