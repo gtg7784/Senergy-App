0d42567a6fdf4f68d041f155921794840aa8ea82
gtg7784 <gtg7784@naver.com>
2020-06-29 22:46:32 +0900

    update image constants

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,4 +2,5 @@ export default {
   mainIllust: require('../assets/illust1.png'),
   cameraIcon: require('../assets/cameraIcon.png'),
   arrowDown: require('../assets/arrowDown.png'),
+  arrowRight: require('../assets/arrowRight.png'),
 };
