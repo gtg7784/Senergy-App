29b211d27289919ef5ed2b6d233d35f0bcd9a2ac
gtg7784 <gtg7784@naver.com>
2020-07-02 19:45:21 +0900

    update api sleep section

--- src/api/sleep.ts
+++ src/api/sleep.ts
@@ -1,5 +1,37 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const sleep = {};
+const sleep = {
+  sleepList: async () => {
+    const token = await AsyncStorage.getItem('token');
+    const response = axios.get('/sleep', {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    return response;
+  },
+  sleepCreate: async () => {
+    const token = await AsyncStorage.getItem('token');
+    const response = axios.post('/sleep', {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    return response;
+  },
+  sleep_read: async (id: number) => {
+    const token = await AsyncStorage.getItem('token');
+    const response = axios.get(`/sleep/${id}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    return response;
+  },
+};
 
 export default sleep;
