157a76fa1b53164f42ea794af69fca926fde90ad
gtg7784 <gtg7784@naver.com>
2020-07-02 13:45:55 +0900

    update users

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,23 +1,44 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const user = {
-  userList: () => {
-    const response = axios.get('/user');
+  userList: async () => {
+    const token = await AsyncStorage.getItem('token');
+    const response = axios.get('/user', {
+      headers: {
+        Authorization: token,
+      },
+    });
 
     return response;
   },
-  userPartialUpdate: (data: FormData) => {
-    const response = axios.patch('/user', data);
+  userPartialUpdate: async (data: FormData) => {
+    const token = await AsyncStorage.getItem('token');
+    const response = axios.patch('/user', data, {
+      headers: {
+        Authorization: token,
+      },
+    });
 
     return response;
   },
-  userTokenAuthCreate: (data: FormData) => {
-    const response = axios.post('/user/token/auth/', data);
+  userTokenAuthCreate: async (data: FormData) => {
+    const token = await AsyncStorage.getItem('token');
+    const response = axios.post('/user/token/auth/', data, {
+      headers: {
+        Authorization: token,
+      },
+    });
 
     return response;
   },
-  user_token_refresh_create: (data: FormData) => {
-    const response = axios.post('/user/token/refresh/', data);
+  user_token_refresh_create: async (data: FormData) => {
+    const token = await AsyncStorage.getItem('token');
+    const response = axios.post('/user/token/refresh/', data, {
+      headers: {
+        Authorization: token,
+      },
+    });
 
     return response;
   },
