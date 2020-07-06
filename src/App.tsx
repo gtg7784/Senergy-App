e2132bcdeab8765b523f839362dc143aab221129
gtg7784 <gtg7784@naver.com>
2020-07-06 20:32:26 +0900

    app tsx class component -> functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import OneSignal from 'react-native-onesignal';
 import {oneSignalId} from 'react-native-dotenv';
@@ -17,18 +17,14 @@ declare const global: {HermesInternal: null | {}};
 const MainStack = createStackNavigator<MainParamList>();
 
 interface Props {}
-interface State {}
 
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+const App: React.FC<Props> = () => {
+  axios.defaults.baseURL =
+    'http://ec2-52-78-109-103.ap-northeast-2.compute.amazonaws.com:8000';
+  axios.defaults.timeout = 10000;
+  axios.defaults.headers.common.Accept = '*/*';
 
-    axios.defaults.baseURL =
-      'http://ec2-52-78-109-103.ap-northeast-2.compute.amazonaws.com:8000/';
-    axios.defaults.timeout = 10000;
-    axios.defaults.headers.common.Accept = '*/*';
-  }
-  componentDidMount() {
+  useEffect(() => {
     // OSNotificationDisplayTypeNone = 0
     // OSNotificationDisplayTypeInAppAlert = 1
     // OSNotificationDisplayTypeNotification = 2
@@ -36,28 +32,29 @@ class App extends React.Component<Props, State> {
       kOSSettingsKeyAutoPrompt: false,
       kOSSettingsKeyInAppLaunchURL: false,
       kOSSettingsKeyInFocusDisplayOption: 2,
+      kOSSSettingsKeyPromptBeforeOpeningPushURL: true,
     });
     OneSignal.inFocusDisplaying(2);
 
     console.log('INIT ONESIGNAL');
 
-    OneSignal.addEventListener('received', this.onReceived);
-    OneSignal.addEventListener('opened', this.onOpened);
-    OneSignal.addEventListener('ids', this.onIds);
-  }
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
 
-  componentWillUnmount() {
-    console.log('UNINIT ONESIGNAL');
-    OneSignal.removeEventListener('received', this.onReceived);
-    OneSignal.removeEventListener('opened', this.onOpened);
-    OneSignal.removeEventListener('ids', this.onIds);
-  }
+    return () => {
+      console.log('UNINIT ONESIGNAL');
+      OneSignal.removeEventListener('received', onReceived);
+      OneSignal.removeEventListener('opened', onOpened);
+      OneSignal.removeEventListener('ids', onIds);
+    };
+  }, []);
 
-  onReceived(notification: any) {
+  const onReceived = (notification: any) => {
     console.log('Notification received: ', notification);
-  }
+  };
 
-  onOpened = (openResult: any) => {
+  const onOpened = (openResult: any) => {
     console.log('Message: ', openResult.notification.payload.body);
     console.log('Data: ', openResult.notification.payload.additionalData);
     console.log('isActive: ', openResult.notification.isAppInFocus);
@@ -67,33 +64,26 @@ class App extends React.Component<Props, State> {
     console.log(data);
   };
 
-  onIds(device: any) {
+  const onIds = (device: any) => {
     console.log('Device info: ', device);
     // 사용자 정보에 푸시 토큰 업데이트
-  }
-
-  render() {
-    return (
-      <>
-        <StatusBar barStyle="light-content" />
-        <NavigationContainer>
-          <MainStack.Navigator headerMode={'none'}>
-            <MainStack.Screen name={'LoginScreen'} component={LoginScreen} />
-            <MainStack.Screen
-              name={'RegisterScreen'}
-              component={RegisterScreen}
-            />
-            <MainStack.Screen name={'MainScreen'} component={MainScreen} />
-            <MainStack.Screen name={'DetailScreen'} component={DetailScreen} />
-            <MainStack.Screen
-              name={'SettingScreen'}
-              component={SettingScreen}
-            />
-          </MainStack.Navigator>
-        </NavigationContainer>
-      </>
-    );
-  }
-}
-
+  };
+  return (
+    <>
+      <StatusBar barStyle={'light-content'} />
+      <NavigationContainer>
+        <MainStack.Navigator headerMode={'none'}>
+          <MainStack.Screen name={'LoginScreen'} component={LoginScreen} />
+          <MainStack.Screen
+            name={'RegisterScreen'}
+            component={RegisterScreen}
+          />
+          <MainStack.Screen name={'MainScreen'} component={MainScreen} />
+          <MainStack.Screen name={'DetailScreen'} component={DetailScreen} />
+          <MainStack.Screen name={'SettingScreen'} component={SettingScreen} />
+        </MainStack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
 export default App;
